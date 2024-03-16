b2a3a90ef52dfdfcfdc80fd4096f2eb6d2ac3090
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-03-15 19:28:16 -0600

    version 1.4

--- backend-roomfinder/src/model/postgresql/propertie.js
+++ backend-roomfinder/src/model/postgresql/propertie.js
@@ -3,14 +3,14 @@ import { LessorsModel } from './lessor.js'
 
 export class PropertiesModel extends Database {
 
-    constructor({ id, lessor_id, type_house, title, description, street, zip, suburb, municipality, state, lat, lgn, availability, price, created_date }) {
+    constructor({ id, lessor_id, type_house, title, description, domicilie, zip, suburb, municipality, state, lat, lgn, availability, price, created_date }) {
         super();
         this.id = id;
         this.lessor_id = lessor_id;
         this.type_house = type_house;
         this.title = title;
         this.description = description;
-        this.street = street;
+        this.domicilie = domicilie;
         this.zip = zip;
         this.suburb = suburb;
         this.municipality = municipality;
@@ -23,23 +23,23 @@ export class PropertiesModel extends Database {
     }
 
     static async getAll() {
-        const [properties] = await this.query(
+        const properties = await this.query(
             "SELECT * FROM estate;"
         );
         return properties.map((property) => new PropertiesModel(property));
     }
 
     static async getByLessor({ lessor_id }) {
-        const [properties] = await this.query(
-            'SELECT * FROM estate WHERE lessor_id = ?;',
+        const properties = await this.query(
+            'SELECT * FROM estate WHERE lessor_id = $1;',
             [lessor_id]
         );
         return properties.map((property) => new PropertiesModel(property))
     }
 
     static async getById({ id }) {
-        const [property] = await this.query(
-            "SELECT * FROM estate WHERE id = ?;",
+        const property = await this.query(
+            "SELECT * FROM estate WHERE id = $1;",
             [id]
         );
         return property[0] ? new PropertiesModel(property[0]) : null;
@@ -47,16 +47,16 @@ export class PropertiesModel extends Database {
 
     static async create({ input }) {
         try {
-            const { lessor_id, type_house, title, description, street, zip, suburb, municipality, state, lat, lgn, availability, price } = input
+            const { lessor_id, type_house, title, description, domicilie, zip, suburb, municipality, state, lat, lgn, availability, price } = input
             const validateLessor = await LessorsModel.getById({ id: lessor_id })
-            if (validateLessor == null) return false;
+            if (validateLessor === null) return false;
 
-            const [result] = await this.query(
-                'INSERT INTO estate (lessor_id, type_house, title, description, street, zip, suburb, municipality, state, lat, lgn, availability, price) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? );',
-                [lessor_id, type_house, title, description, street, zip, suburb, municipality, state, lat, lgn, availability, price]
+            const result = await this.query(
+                'INSERT INTO estate (lessor_id, type_house, title, description, domicilie, zip, suburb, municipality, state, lat, lgn, availability, price) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13 ) RETURNING id;',
+                [lessor_id, type_house, title, description, domicilie, zip, suburb, municipality, state, lat, lgn, availability, price]
             );
 
-            const id = result.insertId
+            const id = result[0].id;
             const newProperty = await this.getById({ id })
 
             return newProperty;
@@ -67,11 +67,15 @@ export class PropertiesModel extends Database {
 
     static async delete({ id }) {
         try {
-            const [property] = await this.query(
-                'DELETE FROM estate WHERE id = ?;',
+            const validate = await this.getById({ id });
+            if (!validate) return false;
+
+            // Eliminar propiedad
+            await this.query(
+                'DELETE FROM estate WHERE id = $1;',
                 [id]
             )
-            return property.affectedRows > 0;
+            return true;
         } catch (error) {
             throw new Error(`Error deleting property: ${error.message}`);
         }
@@ -79,7 +83,7 @@ export class PropertiesModel extends Database {
 
     static async update({ id, input }) {
         try {
-            const { type_house, title, description, street, zip, suburb, municipality, state, lat, lgn, availability, price } = input
+            const { type_house, title, description, domicilie, zip, suburb, municipality, state, lat, lgn, availability, price } = input
             const property = await this.getById({ id })
             if (property === null) return false;
 
@@ -87,7 +91,7 @@ export class PropertiesModel extends Database {
                 type_house,
                 title,
                 description,
-                street,
+                domicilie,
                 zip,
                 suburb,
                 municipality,
@@ -98,14 +102,16 @@ export class PropertiesModel extends Database {
                 price
             })
                 .filter(([key, value]) => value !== undefined)
-                .map(([key, value]) => `${key} = ?`)
+                .map(([key, value]) => {
+                    return `${key} ? $${Object.keys(input).indexOf(key) + 1}`;
+                })
                 .join(', ');
 
             const updateValues = Object.value({
                 type_house,
                 title,
                 description,
-                street,
+                domicilie,
                 zip,
                 suburb,
                 municipality,
@@ -119,7 +125,7 @@ export class PropertiesModel extends Database {
 
             if (updateValues.length !== 0) {
                 await this.query(
-                    `UPDATE estate SET ${this.update} WHERE id = ?;`,
+                    `UPDATE estate SET ${updateColumns} WHERE id = $${updateValues.length + 1};`,
                     [...updateValues, id]
                 );
             }
