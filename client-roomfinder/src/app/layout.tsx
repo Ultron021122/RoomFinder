18f4a18cc49facd35e3edf16076d00ffcaace15d
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-03-31 22:06:42 -0600

    version 1.8.1.1

--- client-roomfinder/src/app/layout.tsx
+++ client-roomfinder/src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import Navigate from "@/components/Navbar";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Providers from "./Providers";
+import Providers from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
