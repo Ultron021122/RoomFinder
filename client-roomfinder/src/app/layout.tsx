2586fea2f06ee3ee2411d58fd39fdd57a144363e
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-04-09 13:01:08 -0500

    version 1.9.1

--- client-roomfinder/src/app/layout.tsx
+++ client-roomfinder/src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
 import { ToastContainer } from "react-toastify";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
