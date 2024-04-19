fd7865206f6c6999e807eb7161a4fa30dda4c3d8
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-04-19 14:26:37 -0500

    version 2.0.6

--- client-roomfinder/src/app/page.tsx
+++ client-roomfinder/src/app/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from 'next';
 import Inicio from "@/components/Main";
+
+export const metadata: Metadata = {
+  title: 'Inicio | RoomFinder',
+}
 
 export default function Home() {
   return (
