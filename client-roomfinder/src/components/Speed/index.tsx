1f678847049da30d6c3d980e10df4705b42cc2b4
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-04-27 00:54:41 -0500

    version 2.1.6

---
+++ client-roomfinder/src/components/Speed/index.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
+import SaveIcon from '@mui/icons-material/Save';
+import PrintIcon from '@mui/icons-material/Print';
+import ShareIcon from '@mui/icons-material/Share';
+import MapIcon from '@mui/icons-material/Map';
+
+const actions = [
+    { icon: <FileCopyIcon />, name: 'Copy' },
+    { icon: <SaveIcon />, name: 'Save' },
+    { icon: <PrintIcon />, name: 'Print' },
+    { icon: <ShareIcon />, name: 'Share' },
+    { icon: <MapIcon />, name: 'Map' }
+];
+
+export default function BasicSpeedDial() {
+    return (
+        <SpeedDial
+            ariaLabel="SpeedDial basic example"
+            sx={{ position: 'absolute', bottom: 16, right: 16, }}
+            icon={<SpeedDialIcon />}
+        >
+            {actions.map((action) => (
+                <SpeedDialAction
+                    key={action.name}
+                    icon={action.icon}
+                    tooltipTitle={action.name}
+                />
+            ))}
+        </SpeedDial>
+    );
+}
