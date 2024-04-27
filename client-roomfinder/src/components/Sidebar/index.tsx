cff6cd811949b7f1cc14203e530ec6afd1a1288f
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-04-27 00:54:41 -0500

    version 2.1.6

--- client-roomfinder/src/components/Sidebar/index.tsx
+++ client-roomfinder/src/components/Sidebar/index.tsx
@@ -3,6 +3,7 @@ import { universities } from "@/utils/constants";
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { FormControl, InputLabel, FormHelperText } from "@mui/material";
 import MenuItem from '@mui/material/MenuItem';
+import BasicSpeedDial from "../Speed";
 
 interface SidebarProps {
     onUniversityChange: (university: string) => void;
@@ -47,8 +48,9 @@ const Sidebar: React.FC<SidebarProps> = ({ onUniversityChange }) => {
             <aside id="logo-sidebar" className="fixed top-0 left-0 z-40 w-56 md:w-60 lg:w-64 min-h-screen pt-[65px] transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-950 dark:border-gray-950" aria-label="Sidebar">
                 <div className="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-950">
                     <h5 className="flex items-center py-2 mt-3 mb-1 text-gray-900 rounded-lg dark:text-white group">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-5 h-5 text-gray-950 dark:text-white" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-gray-950 dark:text-white">
+                            <path d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+                            <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z" />
                         </svg>
                         <span className="ms-2">Búsqueda de Inmueble</span>
                     </h5>
@@ -331,6 +333,7 @@ const Sidebar: React.FC<SidebarProps> = ({ onUniversityChange }) => {
                     </ul>
                 </div>
             </aside >
+            <BasicSpeedDial />
         </>
     );
 }
