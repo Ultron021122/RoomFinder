c78be80f8433ea523d3b867783cc41010979d3a5
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-05-04 21:17:05 -0500

    version 2.1.9

--- backend-roomfinder/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-require("dotenv/config");
-var _app = _interopRequireDefault(require("./app.js"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const PORT = process.env.PORT ?? 3000;
-_app.default.listen(PORT, () => {
-  console.log(`Server on port ${PORT}`);
-});
--- client-roomfinder/src/app/api/cloudinary.js
+++ client-roomfinder/src/app/api/cloudinary.js
@@ -3,8 +3,8 @@ import { v2 as cloudinary } from 'cloudinary';
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
+    api_key: process.env.CLOUD_API_KEY,
+    api_secret: process.env.CLOUD_API_SECRET,
 });
 
 export async function uploadImage(image, folder, transformationOptions = {}) {
