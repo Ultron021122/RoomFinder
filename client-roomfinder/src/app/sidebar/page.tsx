501666e3af68e9c7442554483ac8d1381304dd2e
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-05-12 20:23:27 -0500

    version 3.1.2

---
+++ client-roomfinder/src/app/sidebar/page.tsx
@@ -0,0 +1,25 @@
+import Sidebar, { SidebarItem } from "@/components/Navegate";
+import { BarChart3, Boxes, LayoutDashboard, LifeBuoy, Package, Receipt, Settings, UserCircle } from "lucide-react";
+
+export default function Page() {
+  return (
+    <>
+        <Sidebar>
+            <SidebarItem 
+                icon={<LayoutDashboard size={20} />}
+                text="Dashboard"
+                active
+                alert
+            />
+            <SidebarItem icon={<BarChart3 size={20} />} text="Statistics" alert />
+            <SidebarItem icon={<UserCircle size={20} />} text="Users" />
+            <SidebarItem icon={<Boxes size={20} />} text="Inventory" />
+            <SidebarItem icon={<Package size={20} />} text="Orders" />
+            <SidebarItem icon={<Receipt size={20} />} text="Billings" />
+            <hr className="my-3" />
+            <SidebarItem icon={<Settings size={20} />} text="Settings" />
+            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" alert />
+        </Sidebar>
+    </>
+  );
+}
