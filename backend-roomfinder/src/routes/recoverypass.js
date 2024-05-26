b410ee76110806958ef7c8bb45ed18870eefb9f2
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-05-26 00:14:31 -0500

    version 3.2.2

---
+++ backend-roomfinder/src/routes/recoverypass.js
@@ -0,0 +1,77 @@
+import { Router } from "express";
+import { param, validationResult } from "express-validator";
+import { RecoveryPassController } from "../controller/recoverypass.js";
+
+export const createRecoveryPassRouter = ({ recoveryPassModel }) => {
+    const recoveryPassRouter = Router();
+    const recoveryPassController = new RecoveryPassController({ recoveryPassModel });
+
+    /**
+     * @swagger
+     * tags:
+     *  name: RecoveryPass
+     *  description: RecoveryPass routes management
+     */
+
+    recoveryPassRouter.get('/', recoveryPassController.getAll);
+    recoveryPassRouter.get('/:id', [
+        // Validation
+        param('id').isInt().withMessage('id must be an integer'),
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            next();
+        },
+        recoveryPassController.getById
+    ])
+    recoveryPassRouter.get('/token/:token', [
+        // Validation
+        param('token').isString().withMessage('token must be a string'),
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            next();
+        },
+        recoveryPassController.getByToken
+    ])
+    recoveryPassRouter.get("/user/:id", [
+        param("id").isInt().withMessage('id must be an integer'),
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            next();
+        },
+        recoveryPassController.getByUser
+    ]);
+    recoveryPassRouter.post("/", recoveryPassController.create);
+    recoveryPassRouter.put("/:id", [
+        param("id").isInt().withMessage('id must be an integer'),
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            next();
+        },
+        recoveryPassController.update
+    ]);
+    recoveryPassRouter.delete("/:id", [
+        param("id").isInt().withMessage('id must be an integer'),
+        (req, res, next) => {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+            next();
+        },
+        recoveryPassController.delete
+    ]);
+
+    return recoveryPassRouter;
+}
