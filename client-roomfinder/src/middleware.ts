50d9de3cf8185b14cbe300a9a3343b6441a9635b
Sebastian Martinez Lopez <sebastian.martinez7643@alumnos.udg.mx>
2024-06-09 21:10:49 -0500

    version x.x

--- client-roomfinder/src/middleware.ts
+++ client-roomfinder/src/middleware.ts
@@ -1,10 +1,31 @@
 export { default } from "next-auth/middleware";
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  console.log('Middleware:', pathname)
+  // Block access to /api routes
+  if (pathname.startsWith('/api')) {
+    console.log(request.headers)
+    const internalHeader = request.headers.get('x-internal-request');
+    console.log(internalHeader)
+    console.log(process.env.INTERNAL_REQUEST_SECRET)
+    if (internalHeader !== process.env.INTERNAL_REQUEST_SECRET) {
+      console.log('Access denied')
+      return NextResponse.json({ error: 'Access denied' }, { status: 403 });
+    }
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
   matcher: [
     "/arrendadores",
     "/dashboard/:path*",
     "/admin",
-    "/user/:path*"
+    "/user/:path*",
+    '/api/:path*',
   ],
 };
