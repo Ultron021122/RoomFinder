2fee3c4af542d367037f212d8bd2cae6376067d2
Ultron021122 <marvelsml25@gmail.com>
2024-06-11 00:19:17 -0600

    version 3.3.1

--- client-roomfinder/src/components/Main/Card/index.tsx
+++ client-roomfinder/src/components/Main/Card/index.tsx
@@ -1,4 +1,11 @@
-import { Button, Image } from "@nextui-org/react";
+'use client';
+// Element: CardOwner
+import { useState, useEffect } from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 
 interface CardOwnerProps {
     id: number;
@@ -8,16 +15,68 @@ interface CardOwnerProps {
 }
 
 export const CardOwner = ({ id, title, description, image }: CardOwnerProps) => {
+    const [darkMode, setDarkMode] = useState<boolean>(false);
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+    // Dark Mode 
+    useEffect(() => {
+        const darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+        const setDarkModeFromMediaQuery = () => setDarkMode(darkModeMediaQuery.matches);
+
+        setDarkModeFromMediaQuery();
+        darkModeMediaQuery.addEventListener('change', setDarkModeFromMediaQuery);
+
+        setIsLoaded(true);
+
+        return () => {
+            darkModeMediaQuery.removeEventListener('change', setDarkModeFromMediaQuery);
+        };
+    }, []);
+
+    if (!isLoaded) {
+        return null;
+    }
+
     return (
-        <div className="bg-gray-100 dark:bg-gray-800 p-5 rounded-lg shadow-lg">
-            <Image src={image} alt={title} className="w-full h-auto" />
-            <div className="pt-2 ">
-                <h4 className="dark:text-white text-lg font-semibold">{title}</h4>
-                <p className="dark:text-gray-300">{description}</p>
-                <Button color="primary" className="w-full mt-2">
-                    Ver propiedad
+        <Card
+            sx={{
+                maxWidth: '100%', // Hacer que la tarjeta sea responsiva
+                backgroundColor: darkMode ? '#111827' : '#fff',
+                color: darkMode ? '#fff' : '#111827'
+            }}
+        >
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    height="275"
+                    image={image}
+                    alt={title}
+                    sx={{ width: '100%', height: 275, objectFit: 'cover', objectPosition: 'center' }} // Ajustar la imagen al ancho de la tarjeta
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: darkMode ? '#d1d5db' : '#111827' }}>
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center'
+                }}
+            >
+                <Button
+                    color="primary"
+                    variant='outlined'
+                    href={`/owner/${id}`}
+                    sx={{ width: '100%' }}
+                >
+                    Ver más
                 </Button>
-            </div>
-        </div>
+            </CardActions>
+        </Card>
     );
-}   
+}
