606f42852063f6672da30ebae60508d2170453be
Ultron021122 <marvelsml25@gmail.com>
2024-06-22 23:48:35 -0600

    version 3.4.2

--- client-roomfinder/src/app/api/users/recover/[token]/route.js
+++ client-roomfinder/src/app/api/users/recover/[token]/route.js
@@ -1,26 +1,33 @@
 import { NextResponse } from 'next/server';
 import axios from 'axios';
 
-export async function GET(request, { params }) {
+export async function GET({ params }) {
     const token = params.token;
     try {
         const response = await axios.get(`${process.env.REST_URL}/recovery/token/${token}`);
         return NextResponse.json(
-            { message: 'Usuario encontrado', data: response.data },
-            { status: response.status }
+            { data: response.data },
+            { status: 200 }
         );
     } catch (error) {
+        console.log('error', error)
         const statusMessageMap = {
             503: { message: 'Service unavailable' },
             404: { message: 'Usuario no encontrado' },
-            400: { message: error.response.data.message },
+            400: { message: error.message },
             default: { message: 'Error inesperado' },
         };
-        const message = statusMessageMap[error.status] || statusMessageMap.default;
-        console.log(message);
-        return NextResponse.json(
-            { message },
-            { status: error.status }
-        );
+        const message = statusMessageMap[error.response.status] || statusMessageMap.default;
+        if (error.response.status){
+            return NextResponse.json(
+                { message },
+                { status: error.response.status }
+            );
+        } else {
+            return NextResponse.json(
+                { message },
+                { status: 503 }
+            );
+        }
     }
 }
