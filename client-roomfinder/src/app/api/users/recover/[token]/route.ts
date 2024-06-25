89e3debf507baaa9e49a1e5d94a2ca647cbc71c1
Ultron021122 <marvelsml25@gmail.com>
2024-06-25 00:36:46 -0600

    version por actualizar

--- client-roomfinder/src/app/api/users/recover/[token]/route.ts
+++ client-roomfinder/src/app/api/users/recover/[token]/route.ts
@@ -12,7 +12,6 @@ export async function GET(req: NextRequest) {
             { status: 200 }
         );
     } catch (error: any) {
-        console.log('error', error)
         const statusMessageMap: Record<number, { message: string }> = {
             503: { message: 'Service unavailable' },
             404: { message: 'Usuario no encontrado' },
@@ -22,7 +21,38 @@ export async function GET(req: NextRequest) {
         const message = statusMessageMap[error.response?.status] || statusMessageMap[0];
         return NextResponse.json(
             { message },
-            { status: error.response.status as number}
+            { status: error.response?.status ? error.response.status as number : 500}
+        );
+    }
+}
+
+export async function POST(req: NextRequest) {
+    const pathname = req.nextUrl.pathname;
+    const token = pathname.split('/').pop();
+    const { vchpassword, vchconfirm_password } = await req.json();
+
+    try {
+        const findUser = await axios.get(`${process.env.REST_URL}/recovery/token/${token}`);
+        const usuarioid = findUser
+        const response = await axios.post(`${process.env.REST_URL}/users/${usuarioid}`, {
+            vchpassword
+        });
+        return NextResponse.json(
+            { data: response.data },
+            { status: 200 }
+        );
+
+    } catch (error: any) {
+        const statusMessageMap: Record<number, { message: string }> = {
+            409: { message: 'El correo ya está registrado' },
+            400: { message: error.message },
+            0: { message: 'Error al crear el estudiante' },
+        };
+
+        const message = statusMessageMap[error.response?.status] || statusMessageMap[0];
+        return NextResponse.json(
+            { message },
+            { status: error.response?.status ? error.response.status as number : 500}
         );
     }
 }
--- client-roomfinder/src/components/Users/recoverAccount/form/index.tsx
+++ client-roomfinder/src/components/Users/recoverAccount/form/index.tsx
@@ -10,6 +10,8 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import { messages } from "@/utils/constants";
 import axios from "axios";
+import { Link, Spinner } from "@nextui-org/react";
+import { Alert } from "@/utils/alert";
 
 interface UpdatePassword {
     vchpassword: string;
@@ -21,7 +23,7 @@ export default function Form({ token }: { token: string }) {
     const { status } = useSession();
     const router = useRouter();
 
-    const { control, handleSubmit, formState: { errors }, watch, reset } = useForm<UpdatePassword>({ mode: "onChange" });
+    const { register, control, handleSubmit, formState: { errors }, watch, reset } = useForm<UpdatePassword>({ mode: "onChange" });
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [errorSystem, setErrorSystem] = useState<string | null>(null);
 
@@ -56,9 +58,9 @@ export default function Form({ token }: { token: string }) {
         };
 
         try {
-            const response = await axios.post('/api/users/recover', data);
+            const response = await axios.post(`/api/users/recover/${token}`, data);
             setIsLoading(false);
-            if(response.status === 200) {
+            if (response.status === 200) {
                 toast.success(response.data.message.message, {
                     position: "bottom-right",
                     autoClose: 5000,
@@ -81,39 +83,97 @@ export default function Form({ token }: { token: string }) {
             } else {
                 setErrorSystem(Error.response?.data.message);
             }
+            router.push('/');
         } finally {
             setIsLoading(false);
         }
     };
 
+    // Enfocar el input email al cargar la página
+    useEffect(() => {
+        const input = document.getElementById('vchpassword') as HTMLInputElement;
+        input.focus();
+    }, []);
+
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <div className="w-full p-6 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md dark:bg-gray-800 dark:border-gray-700 sm:p-8">
-                    <h2 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                        Cambio de contraseeña
-                    </h2>
-                    <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
-                        <div>
-                            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Password</label>
-                            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                        </div>
-                        <div>
-                            <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm password</label>
-                            <input type="password" name="confirm-password" id="confirm-password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                        </div>
-                        <div className="flex items-start">
-                            <div className="flex items-center h-5">
-                                <input id="newsletter" aria-describedby="newsletter" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
-                            </div>
-                            <div className="ml-3 text-sm">
-                                <label htmlFor="newsletter" className="font-light text-gray-500 dark:text-gray-300">I accept the <a className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terms and Conditions</a></label>
-                            </div>
-                        </div>
-                        <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Reset passwod</button>
-                    </form>
+            {isLoading ?
+                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto h-[100vh] lg:py-0">
+                    <Spinner />
                 </div>
-            </div>
+                :
+                <div className="flex flex-col justify-center items-center px-6 py-8 mx-auto h-[100vh] lg:py-0">
+                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-5 xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                        <h2 className="p-6 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Cambio de contraseña
+                        </h2>
+                        <form className="mt-4 space-y-4 lg:mt-5 md:space-y-5" action="#">
+                            <div>
+                                <label htmlFor="vchpassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nueva Contraseña</label>
+                                <input
+                                    {...register("vchpassword", {
+                                        required: {
+                                            value: true,
+                                            message: messages.vchpassword.required
+                                        },
+                                        minLength: {
+                                            value: 8,
+                                            message: messages.vchpassword.min
+                                        },
+                                        maxLength: {
+                                            value: 16,
+                                            message: messages.vchpassword.max
+                                        }
+                                    })}
+                                    type="password"
+                                    name="vchpassword"
+                                    id="vchpassword"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                    autoComplete="off"
+                                />
+                                {errors?.vchpassword && (
+                                    <Alert message={errors?.vchpassword.message} />
+                                )}
+                            </div>
+                            <div>
+                                <label htmlFor="vchconfirmPassword" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirma Contraseña</label>
+                                <input
+                                {...register("vchconfirmPassword", {
+                                    required: {
+                                        value: true,
+                                        message: messages.confirm_password.required
+                                    },
+                                    minLength: {
+                                        value: 8,
+                                        message: messages.confirm_password.min
+                                    },
+                                    maxLength: {
+                                        value: 16,
+                                        message: messages.confirm_password.max
+                                    },
+                                    validate: validatePasswordConfirmation // Agregar función de validation
+                                })}
+                                    type="password"
+                                    name="vchconfirmPassword"
+                                    id="vchconfirmPassword"
+                                    placeholder="••••••••"
+                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                />
+                            </div>
+                            <div className="flex items-start">
+                                <div className="flex items-center h-5">
+                                    <input id="newsletter" aria-describedby="newsletter" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
+                                </div>
+                                <div className="ml-3 text-sm">
+                                    <label htmlFor="newsletter" className="font-light text-gray-500 dark:text-gray-300">Yo acepto los <Link className="font-medium text-primary-600 hover:underline dark:text-primary-500" href="#">Terminos y Condiciones</Link></label>
+                                </div>
+                            </div>
+                            <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Restablecer Contraseña</button>
+                        </form>
+                    </div>
+                </div>
+            }
         </section>
     );
 }
