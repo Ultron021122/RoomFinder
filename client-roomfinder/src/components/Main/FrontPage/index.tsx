e804a19583247df94eaf92599f1779b1c2192aa3
Ultron021122 <marvelsml25@gmail.com>
2024-06-28 06:54:54 -0600

    version 3.4.8

--- client-roomfinder/src/components/Main/FrontPage/index.tsx
+++ client-roomfinder/src/components/Main/FrontPage/index.tsx
@@ -3,16 +3,16 @@ import { images } from '@/utils/constants';
 export default function FrontPage() {
     return (
         <>
-            <section className="flex flex-col justify-center items-center min-h-[100vh] bg-cover bg-no-repeat bg-center bg-black bg-opacity-30 dark:bg-opacity-45 bg-blend-darken" style={{ backgroundImage: `url(${images[0].url})` }} >
+            <section className="flex flex-col justify-center items-center min-h-[100vh] bg-cover bg-no-repeat bg-center bg-black bg-opacity-45 bg-blend-darken" style={{ backgroundImage: `url(${images[0].url})` }} >
                 <div className="px-4 mx-auto max-w-screen-xl text-center lg:px-12">
-                    <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+                    <a href="#" className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm rounded-full bg-gray-800 text-white hover:bg-gray-700" role="alert">
                         <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">Nuevo</span> <span className="text-sm font-medium">¡Nuevas habitaciones disponibles! Descúbrelo</span>
                         <svg className="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
                     </a>
-                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Encuentra tu habitación ideal</h1>
+                    <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl lg:text-6xl text-white">Encuentra tu habitación ideal</h1>
                     <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">RoomFinder está aquí para ayudarte a encontrar la habitación perfecta cerca de tu universidad. Explora nuestras opciones y haz tu elección hoy mismo.</p>
                     <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                        <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                        <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-900">
                             Ver más
                             <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                         </a>
