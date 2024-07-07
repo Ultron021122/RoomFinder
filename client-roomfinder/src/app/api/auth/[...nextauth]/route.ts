30813c0e42ffc780a4127bd009c1581684749da5
Ultron021122 <marvelsml25@gmail.com>
2024-07-06 23:58:48 -0600

    version 3.5.3

--- client-roomfinder/src/app/api/auth/[...nextauth]/route.ts
+++ client-roomfinder/src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,24 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
+interface SessionInterface {
+    name?: string | null;
+    email?: string | null;
+    image?: string | null;
+    usuarioid: number;
+    vchname: string;
+    vchpaternalsurname: string;
+    vchmaternalsurname: string;
+    vchemail: string;
+    vchpassword: string;
+    dtbirthdate: string;
+    bnstatus: boolean;
+    vchimage: string;
+    roleid: number;
+    created_at: string;
+    sessionid: number;
+}
+
 const handler = NextAuth({
     providers: [
         CredentialsProvider({
@@ -54,13 +72,30 @@ const handler = NextAuth({
             return token;
         },
         async session({ session, token }) {
-            session.user = token.user as any;
+            session.user = token.user as SessionInterface;
             return session;
         },
     },
     events: {
         signOut: async (message) => {
             console.log("signOut", message);
+            const sessionid = message.token?.user as SessionInterface;
+            console.log("sessionid", sessionid.sessionid);
+            try {
+                await fetch(`${process.env.REST_URL}/users/logout/`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({ sessionid: sessionid.sessionid }),
+                });
+            } catch (error) {
+                if (error instanceof Error) {
+                    throw new Error(error.message);
+                } else {
+                    throw new Error("Algo salió mal. Por favor, inténtelo de nuevo");
+                }
+            }
         },
     },
 });
