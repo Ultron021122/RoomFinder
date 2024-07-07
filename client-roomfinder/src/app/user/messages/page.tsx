81e67bc728a3366cd9cf6bed9fab04df45d5efc9
Ultron021122 <marvelsml25@gmail.com>
2024-07-07 00:04:57 -0600

    version 3.5.4

--- client-roomfinder/src/app/user/messages/page.tsx
+++ client-roomfinder/src/app/user/messages/page.tsx
@@ -20,17 +20,17 @@ export default function Messages() {
             <SidebarItem
               icon={<LayoutDashboard size={20} />}
               text="Dashboard"
-              active
+              url="/dashboard/home"
               alert
             />
-            <SidebarItem icon={<BarChart3 size={20} />} text="Statistics" alert />
-            <SidebarItem icon={<UserCircle size={20} />} text="Users" />
-            <SidebarItem icon={<Boxes size={20} />} text="Inventory" />
-            <SidebarItem icon={<Package size={20} />} text="Orders" />
-            <SidebarItem icon={<Receipt size={20} />} text="Billings" />
+            <SidebarItem icon={<BarChart3 size={20} />} text="Statistics" url="/dashboard/home" alert />
+            <SidebarItem icon={<UserCircle size={20} />} text="Users" url="/dashboard/home" />
+            <SidebarItem icon={<Boxes size={20} />} text="Inventory" url="/dashboard/home" />
+            <SidebarItem icon={<Package size={20} />} text="Orders" url="/dashboard/home" />
+            <SidebarItem icon={<Receipt size={20} />} text="Billings" url="/dashboard/home" />
             <hr className="my-3 border-gray-300 dark:border-gray-800" />
-            <SidebarItem icon={<Settings size={20} />} text="Settings" />
-            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" alert />
+            <SidebarItem icon={<Settings size={20} />} text="Settings" url="/dashboard/home" />
+            <SidebarItem icon={<LifeBuoy size={20} />} text="Help" url="/dashboard/home" alert />
           </Sidebar>
         </div>
         <div className="w-full">
