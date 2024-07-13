cbd435524dbdf4b12ec91f433eb8c42e6af0c10f
Ultron021122 <marvelsml25@gmail.com>
2024-07-13 01:49:41 -0600

    version 3.5.6.1

--- client-roomfinder/src/utils/functions.tsx
+++ client-roomfinder/src/utils/functions.tsx
@@ -14,10 +14,10 @@ export const getFullName = ({ vchname, vchpaternalsurname, vchmaternalsurname }:
 
 export const shortName = ({ vchname, vchpaternalsurname, vchmaternalsurname }: fullName) => {
     // Validar que los campos no estén vacíos
-    if (!vchname.trim() || !vchpaternalsurname.trim() || !vchmaternalsurname.trim()) {
+    /*if (!vchname.trim() || !vchpaternalsurname.trim() || !vchmaternalsurname.trim()) {
         //throw new Error("Todos los campos deben ser proporcionados y no pueden estar vacíos.");
         return;
-    }
+    }*/
     let length = vchname.length + vchpaternalsurname.length + vchmaternalsurname.length;
     if (length > 20) {
         return `${vchname} ${vchpaternalsurname.substring(0, 1)}. ${vchmaternalsurname.substring(0, 1)}.`;
