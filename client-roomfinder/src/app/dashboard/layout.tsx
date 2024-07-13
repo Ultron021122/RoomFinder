57eb6470920478ab68e0c1a1ff99578113f6b7b9
Ultron021122 <marvelsml25@gmail.com>
2024-07-13 01:43:49 -0600

    version 3.5.6

--- client-roomfinder/src/app/dashboard/layout.tsx
+++ client-roomfinder/src/app/dashboard/layout.tsx
@@ -1,62 +1,15 @@
-'use client';
+import DashboardLayout from "@/components/Dashboard";
 
-import Sidebar, { SidebarItem } from "@/components/Navegate";
-import { Box, Button, AppBar, IconButton, Toolbar, Typography, CssBaseline } from "@mui/material";
-import { BarChart3, Boxes, ChevronFirst, ChevronLast, Home, LayoutDashboard, LifeBuoy, MenuIcon, Package, Receipt, Settings, Turtle, UserCircle } from "lucide-react";
-import { useSession } from "next-auth/react";
-import { usePathname } from "next/navigation";
-import React, { useState } from "react";
-
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-    const [expanded, setExpanded] = useState(true);
-    const { data: session } = useSession();
-    const pathname = usePathname();
-
+export default function DashboardApp(
+    {
+        children,
+    }: {
+        children: React.ReactNode;
+    }
+) {
     return (
-        <div className="flex flex-col h-[100vh]">
-            <AppBar component="nav" position="static" className="bg-indigo-800">
-                <Toolbar variant="dense">
-                    <IconButton
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                        onClick={() => setExpanded((expanded) => !expanded)}
-                    >
-                        {expanded ? <ChevronFirst /> : <ChevronLast />}
-                    </IconButton>
-                    <div className="flex items-center">
-                        <Home size={20} />
-                        <Typography
-                            variant="h6"
-                            color="inherit"
-                            component="div"
-                            className="ml-2"
-                        >
-                            RoomFinder
-                        </Typography>
-                    </div>
-                </Toolbar>
-            </AppBar>
-            <div className="flex flex-1">
-                <Sidebar expanded={expanded}>
-                    <SidebarItem
-                        icon={<LayoutDashboard size={20} />}
-                        text="Dashboard"
-                        url="/dashboard/home"
-                        alert
-                    />
-                    <SidebarItem icon={<BarChart3 size={20} />} text="Statistics" url="/dashboard/statistics" alert />
-                    <SidebarItem icon={<UserCircle size={20} />} text="Perfil" url="/dashboard/profile" />
-                    <SidebarItem icon={<Boxes size={20} />} text="Inventory" url="/dashboard/inventory" />
-                    <SidebarItem icon={<Package size={20} />} text="Orders" url="/dashboard/orders" />
-                    <SidebarItem icon={<Receipt size={20} />} text="Billings" url="/dashboard/billings" />
-                    <hr className="my-3 border-gray-300 dark:border-gray-800" />
-                    <SidebarItem icon={<Settings size={20} />} text="Settings" url="/dashboard/settings"/>
-                    <SidebarItem icon={<LifeBuoy size={20} />} text="Help" url="/dashboard/help" alert />
-                </Sidebar>
-                <main className="flex-1 p-4">{children}</main>
-            </div>
-        </div>
+        <DashboardLayout>
+            {children}
+        </ DashboardLayout>
     );
 }
---
+++ client-roomfinder/src/utils/functions.tsx
@@ -0,0 +1,28 @@
+interface fullName {
+    vchname: string;
+    vchpaternalsurname: string;
+    vchmaternalsurname: string;
+}
+
+export const getFullName = ({ vchname, vchpaternalsurname, vchmaternalsurname }: fullName) => {
+    // Validar que los campos no estén vacíos
+    if (!vchname.trim() || !vchpaternalsurname.trim() || !vchmaternalsurname.trim()) {
+        throw new Error("Todos los campos deben ser proporcionados y no pueden estar vacíos.");
+    }
+    return `${vchname} ${vchpaternalsurname} ${vchmaternalsurname}`;
+}
+
+export const shortName = ({ vchname, vchpaternalsurname, vchmaternalsurname }: fullName) => {
+    // Validar que los campos no estén vacíos
+    if (!vchname.trim() || !vchpaternalsurname.trim() || !vchmaternalsurname.trim()) {
+        //throw new Error("Todos los campos deben ser proporcionados y no pueden estar vacíos.");
+        return;
+    }
+    let length = vchname.length + vchpaternalsurname.length + vchmaternalsurname.length;
+    if (length > 20) {
+        return `${vchname} ${vchpaternalsurname.substring(0, 1)}. ${vchmaternalsurname.substring(0, 1)}.`;
+    }
+    return `${vchname} ${vchpaternalsurname} ${vchmaternalsurname}`;
+}
+
+export default getFullName;
