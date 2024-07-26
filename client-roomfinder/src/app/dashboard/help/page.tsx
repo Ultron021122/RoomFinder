4d11eba656cbea1ab5c5b2ff7d1e6e2fa9b447e8
Ultron021122 <marvelsml25@gmail.com>
2024-07-25 23:43:47 -0600

    version 3.6.4

--- client-roomfinder/src/app/dashboard/help/page.tsx
+++ client-roomfinder/src/app/dashboard/help/page.tsx
@@ -1,9 +1,14 @@
-import Profile from "@/components/Dashboard/ProfileModern";
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 
 function Help() {
   return (
     <>
-      <Profile />
+      {/* Some modifications */}
+      <div className="h-full max-w-screen-2xl mx-auto bg-zinc-200 dark:bg-gray-900">
+        <div className="mx-auto">
+          <Breadcrumb pageName="Ayuda" />
+        </div>
+      </div>
     </>
   );
 }
