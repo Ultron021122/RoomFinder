16e23f1c6a12f9839884c5281ae808ba75efcb34
Ultron021122 <marvelsml25@gmail.com>
2024-07-27 02:20:11 -0600

    version 3.6.5

--- client-roomfinder/src/components/Dashboard/index.tsx
+++ client-roomfinder/src/components/Dashboard/index.tsx
@@ -3,16 +3,34 @@
 import Sidebar, { SidebarItem } from "@/components/Navegate";
 import { Box, Button, AppBar, IconButton, Toolbar, Typography, CssBaseline } from "@mui/material";
 import { BarChart3, Boxes, ChevronFirst, ChevronLast, Home, LayoutDashboard, LifeBuoy, MenuIcon, Package, Receipt, Settings, Turtle, UserCircle } from "lucide-react";
+import AddHomeOutlinedIcon from '@mui/icons-material/AddHomeOutlined';
 import { useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 import React, { useCallback, useEffect, useState } from "react";
+import { rolesMapping } from "@/utils/constants";
+// Alerts and notifications
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import 'react-perfect-scrollbar/dist/css/styles.css';
+
+interface UserProfile {
+    vchname: string;
+    vchpaternalsurname: string;
+    vchmaternalsurname: string;
+    vchemail: string;
+    vchimage: string;
+    usuarioid: number;
+    sessionid: number;
+    dtbirthdate: string;
+    bnverified: boolean;
+    bnstatus: boolean;
+    roleid: number;
+}
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     const [expanded, setExpanded] = useState(true);
     const [windowWidth, setWindowWidth] = useState(0);
     const { data: session } = useSession();
+    const user = session?.user as UserProfile;
     const pathname = usePathname();
 
     // Update the window width state
@@ -33,6 +51,8 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
     const toggleSidebar = useCallback(() => {
         setExpanded(windowWidth > 640);
     }, [windowWidth]);
+
+    const roleName = rolesMapping[user?.roleid] || 'Desconocido';
 
     return (
         <PerfectScrollbar>
@@ -77,7 +97,14 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
                         <SidebarItem icon={<BarChart3 size={20} />} text="Estadisticas" url="/dashboard/statistics" alert />
                         <SidebarItem icon={<UserCircle size={20} />} text="Perfil" url="/dashboard/profile" />
                         <SidebarItem icon={<Boxes size={20} />} text="Inventario" url="/dashboard/inventory" />
-                        <SidebarItem icon={<Package size={20} />} text="Historial" url="/dashboard/orders" />
+                        {// Show option only for lessors
+                            roleName === 'Arrendador' && (
+                                <>
+                                    <SidebarItem icon={<AddHomeOutlinedIcon style={{fontSize: 20}}/>} text="Publicar" url="/dashboard/publish" />
+                                    <SidebarItem icon={<Package size={20} />} text="Historial" url="/dashboard/orders" />
+                                </>
+                            )
+                        }
                         <SidebarItem icon={<Receipt size={20} />} text="Pagos" url="/dashboard/billings" />
                         <hr className="my-3 border-gray-300 dark:border-gray-800" />
                         <SidebarItem icon={<Settings size={20} />} text="Configuraciones" url="/dashboard/settings" />
