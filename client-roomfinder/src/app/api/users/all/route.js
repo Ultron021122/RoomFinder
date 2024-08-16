7d025225454937af12edb576f16b0be97c946e78
Ultron021122 <marvelsml25@gmail.com>
2024-08-16 01:12:51 -0600

    version 3.7.8

---
+++ client-roomfinder/src/app/api/users/all/route.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+export async function GET() {
+    try {
+        const response = await axios.get(`${process.env.REST_URL}/users/`);
+        console.log(response.data)
+        return NextResponse.json(
+            { data: response.data },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Server error' },
+            { status: 503 }
+        );
+    }
+}
