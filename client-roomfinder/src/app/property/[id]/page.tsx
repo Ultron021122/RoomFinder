f1aa45db8705e7b9c79be5b88a072dddedcb5baa
Sebastián Martínez López <sebastian.martinez7643@alumnos.udg.mx>
2024-09-13 15:18:32 -0600

    Update page.tsx

--- client-roomfinder/src/app/property/[id]/page.tsx
+++ client-roomfinder/src/app/property/[id]/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -15,24 +16,24 @@ export default function Propiedades() {
                     <p className="text-sm leading-4 font-medium text-white sm:text-slate-500 dark:sm:text-slate-400">Entire house</p>
                     </div>
                     <div className="grid gap-4 col-start-1 col-end-3 row-start-1 sm:mb-6 sm:grid-cols-4 lg:gap-6 lg:col-start-2 lg:row-end-6 lg:row-span-6 lg:mb-0">
-                    <img src="https://tailwindcss.com/_next/static/media/beach-house.9b9ee168.jpg" alt="" className="w-full h-60 object-cover rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full" loading="lazy">
-                    <img src="https://tailwindcss.com/_next/static/media/beach-house-interior-2.3672af98.jpg" alt="" className="hidden w-full h-52 object-cover rounded-lg sm:block sm:col-span-2 md:col-span-1 lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy">
-                    <img src="https://tailwindcss.com/_next/static/media/beach-house-interior-1.f151eb56.jpg" alt="" className="hidden w-full h-52 object-cover rounded-lg md:block lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy">
+                    <Image src="https://tailwindcss.com/_next/static/media/beach-house.9b9ee168.jpg" alt="" className="w-full h-60 object-cover rounded-lg sm:h-52 sm:col-span-2 lg:col-span-full" loading="lazy">
+                    <Image src="https://tailwindcss.com/_next/static/media/beach-house-interior-2.3672af98.jpg" alt="" className="hidden w-full h-52 object-cover rounded-lg sm:block sm:col-span-2 md:col-span-1 lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy">
+                    <Image src="https://tailwindcss.com/_next/static/media/beach-house-interior-1.f151eb56.jpg" alt="" className="hidden w-full h-52 object-cover rounded-lg md:block lg:row-start-2 lg:col-span-2 lg:h-32" loading="lazy">
                     </div>
                     <dl className="mt-4 text-xs font-medium flex items-center row-start-2 sm:mt-1 sm:row-start-3 md:mt-2.5 lg:row-start-2">
                     <dt className="sr-only">Reviews</dt>
                     <dd className="text-indigo-600 flex items-center dark:text-indigo-400">
-                        <svg width="24" height="24" fill="none" aria-hidden="true" className="mr-1 stroke-current dark:stroke-indigo-500">
-                        <path d="m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z"  stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                        <svg width="24" height="24" fill="none" ariaHidden="true" className="mr-1 strokeCurrent dark:stroke-indigo-500">
+                        <path d="m12 5 2 5h5l-4 4 2.103 5L12 16l-5.103 3L9 14l-4-4h5l2-5Z"  strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                         <span>4.89 <span className="text-slate-400 font-normal">(128)</span></span>
                     </dd>
                     <dt className="sr-only">Location</dt>
                     <dd className="flex items-center">
-                        <svg width="2" height="2" aria-hidden="true" fill="currentColor" className="mx-3 text-slate-300">
+                        <svg width="2" height="2" ariaHidden="true" fill="currentColor" className="mx-3 text-slate-300">
                         <circle cx="1" cy="1" r="1" />
                         </svg>
-                        <svg width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="mr-1 text-slate-400 dark:text-slate-500" aria-hidden="true">
+                        <svg width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-1 text-slate-400 dark:text-slate-500" ariaHidden="true">
                         <path d="M18 11.034C18 14.897 12 19 12 19s-6-4.103-6-7.966C6 7.655 8.819 5 12 5s6 2.655 6 6.034Z" />
                         <path d="M14 11a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z" />
                         </svg>
