1eba750971d2424ecf9499676cf9e18075fbd956
abraham salas <abraham.salas@alumnos.udg.mx>
2024-09-15 21:27:06 -0600

    drag n drop agregado

--- client-roomfinder/src/components/Dashboard/Publish/FormularioContext.tsx
+++ client-roomfinder/src/components/Dashboard/Publish/FormularioContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useContext, useState } from 'react';
 
 // Define la interfaz para el estado del formulario
-interface Inmueble {
+export interface Inmueble {
     tipoInmueble: string;
     servicios: string[];
     amenidades: string[];
@@ -9,17 +9,33 @@ interface Inmueble {
     numCamas: number;
     numBanos: number;
     numHuespedes: number;
-    fotos: string[];
+    fotos: File[];
     descripcion: string;
     reglas: string;
     costo: number;
     [key : string]: any
 }
 
+// valores por defecto para el inmueble
+const valoresDefecto : Inmueble = {
+    tipoInmueble: '',
+    servicios: [],
+    amenidades: [],
+    numRecamaras: 1,
+    numCamas: 1,
+    numBanos: 1,
+    numHuespedes: 1,
+    fotos: [],
+    descripcion: '',
+    reglas: '',
+    costo: 0,
+}
+
 // Interfaz para el contexto
 interface FormularioContextProps {
     inmueble: Inmueble;
     setInmueble: (data: Partial<Inmueble>) => void;  // Función para actualizar el inmueble
+    reiniciarValores: () => void // función para reiniciar los valores actuales a valores por defecto
 }
 
 // Crear el contexto
@@ -28,27 +44,20 @@ const FormularioContext = createContext<FormularioContextProps | undefined>(unde
 // Proveedor del contexto
 export const FormularioProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     // Definir el estado inicial del inmueble
-    const [inmueble, setInmuebleState] = useState<Inmueble>({
-        tipoInmueble: '',
-        servicios: [],
-        amenidades: [],
-        numRecamaras: 1,
-        numCamas: 1,
-        numBanos: 1,
-        numHuespedes: 1,
-        fotos: [],
-        descripcion: '',
-        reglas: '',
-        costo: 0,
-    });
+    const [inmueble, setInmuebleState] = useState<Inmueble>(valoresDefecto);
 
     // Función para actualizar el estado del inmueble
     const setInmueble = (data: Partial<Inmueble>) => {
         setInmuebleState((prev) => ({ ...prev, ...data }));
     };
 
+    // función para reiniciar los valores del inmueble
+    const reiniciarValores = () => {
+        setInmuebleState(valoresDefecto);
+    }
+
     return (
-        <FormularioContext.Provider value={{ inmueble, setInmueble }}>
+        <FormularioContext.Provider value={{ inmueble, setInmueble, reiniciarValores }}>
             {children}
         </FormularioContext.Provider>
     );
