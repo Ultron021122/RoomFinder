608535108bcf1d5ff3f7f54a4745522b385aec33
Sebastián ML <s.martinez@inntecmp.com.mx>
2024-09-24 18:31:59 -0600

    Update index.tsx

--- client-roomfinder/src/components/List/index.tsx
+++ client-roomfinder/src/components/List/index.tsx
@@ -41,7 +41,7 @@ export default function ListItems() {
         <>
             <PerfectScrollbar className='custom-scrollbar'>
                 <div className='container mx-auto px-4 h-[100vh]'>
-                    <h1 className="dark:text-white text-3xl font-bold py-10 text-center">
+                    <h1 className="dark:text-white text-3xl font-bold pt-14 pb-10 text-center">
                         Arrendadores
                     </h1>
                     {isLoading ?
@@ -79,12 +79,6 @@ export default function ListItems() {
                             )
                         )
                     }
-
-                    <section className='mt-10'>
-                        <p className='dark:text-gray-200'>
-                            Más contenido próximamente
-                        </p>
-                    </section>
                 </div>
             </PerfectScrollbar>
         </>
