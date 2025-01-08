60bc7c8b7aa6c161d78b6a7b6ee64c57a9454a60
Ultron021122 <marvelsml25@gmail.com>
2025-01-08 00:26:28 -0600

    version 5.0.2

--- backend-roomfinder/src/model/postgresql/propertie.js
+++ backend-roomfinder/src/model/postgresql/propertie.js
@@ -1,14 +1,25 @@
 import { Database } from "./database.js";
 import { LessorsModel } from './lessor.js'
 
-export class PropertiesModel extends Database {
+export class PropertiesModel {
 
     constructor({ vchtitle, propertyid, propertytypeid, lessorid, bnavailability, intnumberrooms, intnumberbeds, intnumberbathrooms,
         intmaxoccupacy, bnfurnished, vchfurnituretype, decrentalcost, dtavailabilitydate,
         intmincontractduration, intmaxcontractduration, decpropertyrating,
-        bnstudyzone, vchbuildingsecurity, vchtransportationaccess, objservices, objphotos, objlocation,
+        bnstudyzone, vchbuildingsecurity, vchtransportationaccess,
+        bnwaterincluded, bnelectricityincluded,
+        bninternetincluded, bngasincluded,
+        bnheatingincluded, bnairconditioningincluded,
+        bnlaundryincluded, bnparkingincluded,
+        bncleaningincluded, bncabletvincluded,
+        bnwashingmachineincluded, bnkitchen, bnlivingroom,
+        bndiningroom, bncoolerincluded, bngardenincluded, bnwashingarea,
+        intaccountparking,
+        objphotos,
+        vchexteriornumber, vchinteriornumber, vchstreet,
+        vchaddresscomplement, vchneighborhood, vchmunicipality,
+        vchstateprovince, intzip, vchcountry, lat, lng,
         vchpropertyrules, vchdescription, created_at }) {
-        super();
         this.lessorid = lessorid;
         this.vchtitle = vchtitle;
         this.propertyid = propertyid;
@@ -30,9 +41,38 @@ export class PropertiesModel extends Database {
         this.vchtransportationaccess = vchtransportationaccess;
         this.vchpropertyrules = vchpropertyrules;
         this.vchdescription = vchdescription;
-        this.objservices = objservices;
+        // this.objservices = objservices;
+        this.bnwaterincluded = bnwaterincluded;
+        this.bnelectricityincluded = bnelectricityincluded;
+        this.bninternetincluded = bninternetincluded;
+        this.bngasincluded = bngasincluded;
+        this.bnheatingincluded = bnheatingincluded;
+        this.bnairconditioningincluded = bnairconditioningincluded;
+        this.bnlaundryincluded = bnlaundryincluded;
+        this.bnparkingincluded = bnparkingincluded;
+        this.bncleaningincluded = bncleaningincluded;
+        this.bncabletvincluded = bncabletvincluded;
+        this.bnwashingmachineincluded = bnwashingmachineincluded;
+        this.bnkitchen = bnkitchen;
+        this.bnlivingroom = bnlivingroom;
+        this.bndiningroom = bndiningroom;
+        this.bncoolerincluded = bncoolerincluded;
+        this.bngardenincluded = bngardenincluded;
+        this.bnwashingarea = bnwashingarea;
+        this.intaccountparking = intaccountparking;
         this.objphotos = objphotos;
-        this.objlocation = objlocation;
+        // this.objlocation = objlocation;
+        this.vchexteriornumber = vchexteriornumber;
+        this.vchinteriornumber = vchinteriornumber;
+        this.vchstreet = vchstreet;
+        this.vchaddresscomplement = vchaddresscomplement;
+        this.vchneighborhood = vchneighborhood;
+        this.vchmunicipality = vchmunicipality;
+        this.vchstateprovince = vchstateprovince;
+        this.intzip = intzip;
+        this.vchcountry = vchcountry;
+        this.lat = lat;
+        this.lng = lng;
         this.created_at = created_at;
     }
 
@@ -41,7 +81,7 @@ export class PropertiesModel extends Database {
         const client = await db.pool.connect();
         try {
             const properties = await client.query(
-                `SELECT * FROM "Usuario"."Propiedades";`
+                `SELECT * FROM "Usuario"."vwPropertiesGet";`
             );
             return properties.rows.map((property) => new PropertiesModel(property));
         } finally {
@@ -54,7 +94,7 @@ export class PropertiesModel extends Database {
         const client = await db.pool.connect();
         try {
             const properties = await client.query(
-                `SELECT * FROM "Usuario"."Propiedades" WHERE lessorid = $1;`,
+                `SELECT vp.* FROM "Usuario"."vwPropertiesGet" as vp WHERE vp.lessorid = $1;`,
                 [lessorid]
             );
             return properties.rows.map((property) => new PropertiesModel(property));
@@ -68,7 +108,7 @@ export class PropertiesModel extends Database {
         const client = await db.pool.connect();
         try {
             const property = await client.query(
-                `SELECT * FROM "Usuario"."Propiedades" WHERE propertyid = $1;`,
+                `SELECT vp.* FROM "Usuario"."vwPropertiesGet" as vp WHERE vp.propertyid = $1;`,
                 [id]
             );
             return property.rowCount > 0 ? new PropertiesModel(property.rows[0]) : null;
@@ -79,12 +119,8 @@ export class PropertiesModel extends Database {
 
     static async create({ input }) {
         try {
-            const { vchtitle, propertytypeid, lessorid, bnavailability, intnumberrooms, intnumberbeds, intnumberbathrooms,
-                intmaxoccupacy, bnfurnished, vchfurnituretype, decrentalcost, dtavailabilitydate,
-                intmincontractduration, intmaxcontractduration, decpropertyrating,
-                bnstudyzone, vchbuildingsecurity, vchtransportationaccess, objservices, objphotos, objlocation,
-                vchpropertyrules, vchdescription } = input
-            
+            const { lessorid } = input
+
             const validateLessor = await LessorsModel.getById({ id: lessorid })
             if (validateLessor === null) return false;
 
@@ -93,26 +129,16 @@ export class PropertiesModel extends Database {
 
             try {
                 const result = await client.query(
-                    'CALL "Usuario"."get_user_name_by_id"($1, $2)', [lessorid, null]
+                    'CALL "Usuario"."prSetProperty"($1, $2)', [input, null]
                 );
-                console.log(result.rows[0]);
-                return result.rows[0];
-                // return new PropertiesModel(result.rows[0]);
+
+                const propertyid = result.rows[0].property_id;
+                const newProperty = await this.getById({ id: propertyid });
+
+                return newProperty;
             } finally {
                 client.release();
             }
-
-            // const result = await this.query(
-            //     `INSERT INTO "Usuario"."Propiedades" (vchtitle, lessorid, intnumberrooms, intnumberbathrooms, intmaxoccupacy, bnfurnished, vchfurnituretype, decrentalcost, dtavailabilitydate, intmincontractduration,
-            //         intmaxcontractduration, decpropertyrating, bnstudyzone, vchbuildingsecurity, vchtransportationaccess, vchpropertyrules, vchdescription) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17 ) RETURNING propertyid;`,
-            //     [vchtitle, lessorid, intnumberrooms, intnumberbathrooms, intmaxoccupacy, bnfurnished, vchfurnituretype, decrentalcost, dtavailabilitydate, intmincontractduration,
-            //         intmaxcontractduration, decpropertyrating, bnstudyzone, vchbuildingsecurity, vchtransportationaccess, vchpropertyrules, vchdescription]
-            // );
-
-            // const id = result[0].propertyid;
-            // const newProperty = await this.getById({ id })
-
-            // return newProperty;
         } catch (error) {
             throw new Error(`Error creating property: ${error.message}`)
         }
@@ -143,8 +169,8 @@ export class PropertiesModel extends Database {
         try {
             const { vchtitle, intnumberrooms, intnumberbathrooms, intmaxoccupacy, bnfurnished, vchfurnituretype, decrentalcost, dtavailabilitydate, intmincontractduration,
                 intmaxcontractduration, decpropertyrating, bnstudyzone, vchbuildingsecurity, vchtransportationaccess, vchpropertyrules, vchdescription } = input
-            
-                const property = await this.getById({ id })
+
+            const property = await this.getById({ id })
             if (property === null) return false;
 
             const updateColumns = Object.entries({
