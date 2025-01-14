4b704462c2fee38c01c5e550cbeb671791616619
Ultron021122 <marvelsml25@gmail.com>
2025-01-14 00:20:52 -0600

    version 5.0.5

---
+++ client-roomfinder/src/app/api/properties/[id]/route.js
@@ -0,0 +1,56 @@
+import { NextResponse } from 'next/server';
+import axios from 'axios';
+
+export async function GET(request, { params }) {
+    const id = params.id;
+    try {
+        const response = await axios.get(`${process.env.REST_URL}/properties/${id}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.REST_SECRET}`
+            }
+        });
+        const statusMessageMap = {
+            200: { message: 'Propiedad encontrada', data: response.data },
+            404: { message: 'Propiedad no encontrada' },
+            400: { message: response.data.message },
+            default: { message: 'Error al buscar la propiedad' },
+        };
+        const message = statusMessageMap[response.status] || statusMessageMap.default;
+        return NextResponse.json(
+            { message, data: response.data },
+            { status: response.status }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Server error' },
+            { status: error.response.status }
+        );
+    }
+}
+
+export async function DELETE(request, { params }) {
+    const id = params.id;
+    try {
+        const response = await axios.delete(`${process.env.REST_URL}/properties/${id}`, {
+            headers: {
+                Authorization: `Bearer ${process.env.REST_SECRET}`
+            }
+        });
+        const statusMessageMap = {
+            200: { message: 'Propiedad eliminada', data: response.data },
+            404: { message: 'Propiedad no encontrada' },
+            400: { message: response.data.message },
+            default: { message: 'Error al eliminar la propiedad' },
+        };
+        const message = statusMessageMap[response.status] || statusMessageMap.default;
+        return NextResponse.json(
+            { message, data: response.data },
+            { status: response.status }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { message: 'Server error' },
+            { status: error.response.status }
+        );
+    }
+}
