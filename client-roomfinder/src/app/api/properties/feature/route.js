d71a9721cfc0cb69ead06f3327fc0d4cbc93c4a7
Ultron021122 <marvelsml25@gmail.com>
2025-01-29 00:50:57 -0600

    version 5.2.2

---
+++ client-roomfinder/src/app/api/properties/feature/route.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = await axios.get(`${process.env.REST_URL}/properties/top/featured`, {
+            headers: {
+                Authorization: `Bearer ${process.env.REST_SECRET}`
+            }
+        });
+
+        return NextResponse.json(
+            { data: response.data },
+            { status: 200 }
+        );
+    } catch (error) {
+        return NextResponse.json(
+            { message: error.response.data.message },
+            { status: error.response.status || 503 }
+        );
+    }
+}
