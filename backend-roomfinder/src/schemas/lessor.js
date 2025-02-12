7aba2e755f522f9d117fb1c010f43b44bd680ef5
Ultron021122 <marvelsml25@gmail.com>
2025-02-12 02:08:06 -0600

    version 5.4.4

--- backend-roomfinder/src/schemas/lessor.js
+++ backend-roomfinder/src/schemas/lessor.js
@@ -20,7 +20,7 @@ const lessorSchema = z.object({
     bnstatus: z.boolean(),
     bnverified: z.boolean(),
     vchimage: z.string().url(),
-    vchcoverimage: z.string().optional(),
+    vchcoverimage: z.string().url().optional(),
     roleid: z.number(),
     vchphone: z.string().superRefine((val, ctx) => {
         const phoneRegex = /^\d{10}$/; // Match a 10-digit phone number
