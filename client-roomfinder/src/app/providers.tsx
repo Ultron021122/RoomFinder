b0196800392d662a168224c657c3ca6369d005d3
Ultron021122 <marvelsml25@gmail.com>
2025-02-13 01:36:08 -0600

    version 5.4.5.1.1

--- client-roomfinder/src/app/providers.tsx
+++ client-roomfinder/src/app/providers.tsx
@@ -3,7 +3,6 @@
 import { SessionProvider } from "next-auth/react";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { PropertyProvider } from '@/contexts/PropertyContext';
-
 interface Props {
     children: React.ReactNode;
 }
