a8b019bd1854cf877bd91ecf0baadd4131d45e71
Ultron021122 <marvelsml25@gmail.com>
2025-02-14 01:10:46 -0600

    version 5.5.1

--- client-roomfinder/src/app/privacity/page.tsx
+++ client-roomfinder/src/app/privacity/page.tsx
@@ -2,17 +2,17 @@ import Layout from "@/components/layout";
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-    title: 'Contacto',
+    title: 'Privacidad',
 };
 
-function Contact() {
+function Privacity() {
     return (
         <Layout>
             <section className="pt-20 max-w-7xl">
-                <h1 className="text-neutral-950 dark:text-gray-200">Contacto</h1>
+                <h1 className="text-neutral-950 dark:text-gray-200">Privacity</h1>
             </section>
         </Layout>
     )
 }
 
-export default Contact;
+export default Privacity;
