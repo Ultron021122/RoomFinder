6e971388e7105261eba4555d24914078a1c08125
Ultron021122 <marvelsml25@gmail.com>
2025-02-16 02:06:43 -0600

    version 5.5.1

---
+++ client-roomfinder/src/app/properties/page.tsx
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import SectionProperty from "@/components/properties";
+
+function PropertiesView() {
+    return (
+        <Layout>
+            <h1>Properties</h1>
+            <SectionProperty />
+        </Layout>
+    )
+}
+
+
+export default PropertiesView;
