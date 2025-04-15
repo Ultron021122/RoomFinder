9ae4f5a96fdc1c67d359b5d3f30cfd7fde30b85a
abraham salas <abraham.salas@alumnos.udg.mx>
2025-04-15 17:38:57 -0600

    merge error solved

--- client-roomfinder/src/app/dashboard/rentals/page.tsx
+++ client-roomfinder/src/app/dashboard/rentals/page.tsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-=======
 import RentalHistory from "@/components/Dashboard/rental-history";
-import Rentals from "@/components/Dashboard/rentals"
->>>>>>> 212ad3faf3abc32a1a1caea6754e2b16f535305b
 import { BreadcrumbWithCustomSeparator } from "@/components/GeneralComponents/Breadcrumbs";
 import { Metadata } from "next";
 
@@ -16,10 +12,7 @@ const RentalsPage = () => {
         <div className="h-full max-w-screen-2xl mx-auto">
             <BreadcrumbWithCustomSeparator pageName="Rentas"/>
             <div className="mx-auto">
-<<<<<<< HEAD
-=======
                 <RentalHistory/>
->>>>>>> 212ad3faf3abc32a1a1caea6754e2b16f535305b
             </div>
         </div>
     )
