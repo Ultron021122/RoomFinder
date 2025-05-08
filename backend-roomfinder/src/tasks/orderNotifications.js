093c93fcc8db1ce13d30749531e088ef9d1a7624
Ultron021122 <marvelsml25@gmail.com>
2025-05-07 20:54:41 -0600

    version 6.4.4

--- backend-roomfinder/src/tasks/orderNotifications.js
+++ backend-roomfinder/src/tasks/orderNotifications.js
@@ -1,6 +1,6 @@
 import cron from 'node-cron';
 import moment from 'moment';
-import { EmailService } from './email.js'; 
+import { EmailService } from '../server/email.js'; 
 
 // Crear una instancia de tu servicio de correo
 const emailService = new EmailService();
@@ -10,7 +10,12 @@ const changeStatus = async () => {
   try {
     const hoy = moment();
 
-    const response = await axios.get(`${process.env.ROUTE_LOCAL}/api/tasks`);
+    const response = await axios.get(`${process.env.ROUTE_LOCAL}/api/tasks`, {
+      headers: {
+        'Authorization': `Bearer ${process.env.TOKEN}`
+      }
+    });
+    
     if (response) {
         await emailService.sendNotification(0, 'admin', 'admin@roomfinder.website', hoy.format('YYYY-MM-DD'));
         console.log(`Correo enviado a admin (admin@roomfinder.website)`);
